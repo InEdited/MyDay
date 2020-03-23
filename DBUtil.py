f76bc920bb0e431c9b1bbaa22178dfb14b2a6ea5
Ahmed Alaa <ineditedofficial@gmail.com>
2020-03-23 23:11:25 +0200

    working shit

--- DBUtil.py
+++ DBUtil.py
@@ -9,18 +9,45 @@ def initDB():
     cursor = connection.cursor()
     return cursor
 
-def get_catalog(name):
+def get_sub_catalogs(name):
+    print("yallayaba")
     cursor = initDB()
     #get subcats
     cursor.execute('''
     SELECT c_code FROM catalog where name=?
-    ''',(str(name)))
+    ''',(str(name),))
     catalog_code = cursor.fetchall()
+    #print(catalog_code[0][0])
     cursor.execute('''
     SELECT s_code FROM catalog_has_sub_catalog where c_code=?
-    ''',(str(catalog_code)))
+    ''',(str(catalog_code[0][0]),))
     codes = cursor.fetchall()
+    service_data = []
+    for code in codes:
+        #print(code[0])
+        cursor.execute('''
+        SELECT name FROM sub_catalog where s_code=?
+        ''',(str(code[0]),))
+        service_data.append(cursor.fetchall())
+    print(service_data)
+    return service_data
+
+
+def get_services(name):
+    cursor = initDB()
+    #get subcats
     cursor.execute('''
-    SELECT s_code FROM catalog_has_sub_catalog where c_code=?
-    ''',(str(catalog_code)))
-    codes = cursor.fetchall()
+    SELECT s_code FROM sub_catalog where name=?
+    ''',(str(name),))
+    sub_catalog_code = cursor.fetchall()
+    print(sub_catalog_code[0][0])
+    cursor.execute('''
+    SELECT name FROM 
+    (
+        (service AS s INNER JOIN service_has_sub_catalog AS sc
+        ON s.s_id = sc.s_id)
+    )
+    where sc.s_code=?
+    ''',(str(sub_catalog_code[0][0]),))
+    return cursor.fetchall()
+    
--- main copy.py
+++ main copy.py
@@ -1,5 +1,4 @@
 from flask import Flask , request, render_template
-import DBUtil as db
 
 app = Flask(__name__)
 
@@ -15,22 +14,9 @@ def about():
 def catalog(name):    
     return render_template('catalog.html')
 
-@app.route("/sub_catalogs/<name>")
+@app.rout("/sub_catalogs/<name>")
 def subcatalog(name):
-    print(name)
-    stuff = db.get_sub_catalogs(name)
-    return render_template('sub_catalogs.html', things = stuff)
-
-@app.route("/sub_catalogs/services/<name>")
-def services(name):
-    print(name)
-    stuff = db.get_services(name)
-    return render_template('services.html', things = stuff)
-
-
-@app.route("/debug")
-def debug():
-    db.get_services("Shops")
+    return render_template('sub_catalogs.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=4000)
